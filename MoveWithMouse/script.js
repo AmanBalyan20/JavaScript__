63c1403dd8e7533906376933aa0873cd9be44f9b
AmanBalyan20 <19ca1001@mvn.edu.in>
2023-10-16 21:33:56 +0530

    CursorMovementObject

--- MoveWithMouse/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("mousemove" , function(event){
-    gsap.to('#rectangle' ,{
-        left:event.clientX + 'px',
-        top: event.clientY + 'px',
-        ease : Power3
-    } );
-});
