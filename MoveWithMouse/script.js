8bde95225b23c846f507c39394b1490677fc5828
Aman Balyan <97960179+AmanBalyan20@users.noreply.github.com>
2023-10-19 17:50:53 +0530

    CursorFollower__GSAP

--- MoveWithMouse/script.js
+++ MoveWithMouse/script.js
@@ -1,7 +1,18 @@
+// window.addEventListener("mousemove" , function(event){
+//     gsap.to('#rectangle' ,{
+//         left:event.clientX + 'px',
+//         top: event.clientY + 'px',
+//         ease : Power3
+//     } );
+// });
+
 window.addEventListener("mousemove" , function(event){
+    let  rect = document.querySelector("#rectangle");
+    let valueX  = gsap.utils.mapRange(0, window.innerWidth, 100 + rect.getBoundingClientRect().width/2 , window.innerWidth-(100 + rect.getBoundingClientRect().width/2), event.clientX);
+    let valueY  = gsap.utils.mapRange(0, window.innerHeight, 100 + rect.getBoundingClientRect().width/2 , window.innerHeight-(100 + rect.getBoundingClientRect().width/2), event.clientY);
     gsap.to('#rectangle' ,{
-        left:event.clientX + 'px',
-        top: event.clientY + 'px',
+        left:valueX ,
+        top: valueY,
         ease : Power3
     } );
 });
